6e484e496999c326259a6cb9c135d771894bc557
clebsonf <clebson.augusto@dcx.ufpb.br>
2020-11-05 03:17:58 -0300

    feat: add spring basic skeleton

---
+++ crud-music/src/main/java/com/clebson/appmusic/crud/CrudMusicApplication.java
@@ -0,0 +1,13 @@
+package com.clebson.appmusic.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMusicApplication.class, args);
+	}
+
+}
---
+++ crud-music/src/test/java/com/clebson/appmusic/crud/CrudMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clebson.appmusic.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
